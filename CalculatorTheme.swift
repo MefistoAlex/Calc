1485bf4e6820d9cca11737e1eca72068025f605e
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-12-11 15:06:52 +0200

    created CalculatorTheme with store coolers hex values

---
+++ CalculatorTheme.swift
@@ -0,0 +1,22 @@
+//
+//  CalculatorTheme.swift
+//  Calc
+//
+//  Created by Alexandr Mefisto on 11.12.2022.
+//
+
+import UIKit
+
+struct CalculatorTheme {
+    let backgroundColor: String
+    let diaplayLableColor: String
+
+    let pinpadTintColor: String
+    let pinpadTitleColor: String
+
+    let operationsTintColor: String
+    let operationsTitleColor: String
+
+    let extraFunctionsTintColor: String
+    let extraFunctionsTitleColor: String 
+}
