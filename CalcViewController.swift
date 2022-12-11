69a485db387e51c230cc66d1989564a4a3b50412
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-12-11 17:43:12 +0200

    code refactoring

--- CalcViewController.swift
+++ CalcViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CalcViewController.swift
 //  Calc
 //
 //  Created by Alexandr Mefisto on 10.12.2022.
@@ -7,12 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalcViewController: UIViewController {
     // MARK: - IBOutlets
+
+    //  - LCDDysplay
 
     @IBOutlet var lcdDisplay: UIView!
     @IBOutlet var displayLabel: UILabel!
+
     // pin pad buttons
+
     @IBOutlet var pinpadButton0: UIButton!
     @IBOutlet var pinpadButton1: UIButton!
     @IBOutlet var pinpadButton2: UIButton!
@@ -24,16 +28,22 @@ class ViewController: UIViewController {
     @IBOutlet var pinpadButton8: UIButton!
     @IBOutlet var pinpadButton9: UIButton!
     @IBOutlet var decimalButton: UIButton!
-    // extra functions
-    @IBOutlet var clearButton: UIButton!
-    @IBOutlet var negateButton: UIButton!
-    @IBOutlet var persentageButton: UIButton!
-    // operations
+
+    //  operations
+
     @IBOutlet var equalButton: UIButton!
     @IBOutlet var divideButton: UIButton!
     @IBOutlet var multiplyButton: UIButton!
     @IBOutlet var addButton: UIButton!
     @IBOutlet var subtractButton: UIButton!
+
+    // extra functions
+
+    @IBOutlet var clearButton: UIButton!
+    @IBOutlet var negateButton: UIButton!
+    @IBOutlet var persentageButton: UIButton!
+
+    // MARK: - Color Themes
 
     private var currentTheme: CalculatorTheme {
         CalculatorTheme(
@@ -55,35 +65,34 @@ class ViewController: UIViewController {
         decotrateView()
     }
 
-    // MARK: - Privates
+    // MARK: - Decorate
 
     private func decotrateView() {
-        //view colors
+        // view colors
         let backgroundColor = UIColor(hex: currentTheme.backgroundColor)
         let diaplayLableColor = UIColor(hex: currentTheme.diaplayLableColor)
-        //pinpad colors
+        // pinpad colors
         let pinpadTintColor = UIColor(hex: currentTheme.pinpadTintColor)
         let pinpadTitleColor = UIColor(hex: currentTheme.pinpadTitleColor)
         // operations colors
         let operationsTintColor = UIColor(hex: currentTheme.operationsTintColor)
         let operationsTitleColor = UIColor(hex: currentTheme.operationsTitleColor)
-        //extra func colors
+        // extra func colors
         let extraFunctionsTintColor = UIColor(hex: currentTheme.extraFunctionsTintColor)
         let extraFunctionsTitleColor = UIColor(hex: currentTheme.extraFunctionsTitleColor)
 
-        //view setting
+        // view setting
         view.backgroundColor = backgroundColor
         lcdDisplay.backgroundColor = .clear
         displayLabel.textColor = diaplayLableColor
-        
-        //buttons setting
+
+        // buttons setting
         decoratePinPad(tintColor: pinpadTintColor, titleColor: pinpadTitleColor)
         decorateOperations(tintColor: operationsTintColor, titleColor: operationsTitleColor)
         decorateExtraFunctions(tintColor: extraFunctionsTintColor, titleColor: extraFunctionsTitleColor)
-        
     }
 
-    private func decorateButton(_ button: UIButton, tintColor: UIColor?, titleColor: UIColor?, isSliced:Bool = false) {
+    private func decorateButton(_ button: UIButton, tintColor: UIColor?, titleColor: UIColor?, isSliced: Bool = false) {
         if isSliced {
             button.setBackgroundImage(UIImage(named: "CircleSliced"), for: .normal)
         } else {
@@ -93,7 +102,6 @@ class ViewController: UIViewController {
         button.setTitleColor(titleColor, for: .normal)
         button.backgroundColor = .clear
         button.titleLabel?.font = UIFont.systemFont(ofSize: 40)
-        
     }
 
     private func decorateExtraFunctions(tintColor: UIColor?, titleColor: UIColor?) {
@@ -122,5 +130,41 @@ class ViewController: UIViewController {
         decorateButton(pinpadButton8, tintColor: tintColor, titleColor: titleColor)
         decorateButton(pinpadButton9, tintColor: tintColor, titleColor: titleColor)
         decorateButton(decimalButton, tintColor: tintColor, titleColor: titleColor)
+    }
+
+    // MARK: - IBActions
+
+    @IBAction private func clearPressed() {
+    }
+
+    @IBAction private func negatePressed() {
+    }
+
+    @IBAction private func percentagePressed() {
+    }
+
+    // MARK: - Operations
+
+    @IBAction private func addPressed() {
+    }
+
+    @IBAction private func minusPressed() {
+    }
+
+    @IBAction private func multiplyPressed() {
+    }
+
+    @IBAction private func dividePressed() {
+    }
+
+    @IBAction private func equalsPressed() {
+    }
+
+    // MARK: - Number Input
+
+    @IBAction private func decimalPressed() {
+    }
+
+    @IBAction private func numberPressed(_ sender: UIButton) {
     }
 }
