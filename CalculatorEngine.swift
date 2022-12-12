f88fe0f84f7c398be501054f5032afaac0e904bd
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-12-12 18:02:40 +0100

    added OperationSide enum and operationSide property
    added mathEquation property and connected to the math operations
    
    added lcdDisplayText property

--- CalculatorEngine.swift
+++ CalculatorEngine.swift
@@ -8,6 +8,23 @@
 import Foundation
 
 struct CalculatorEngine {
+    enum OperationSide {
+        case leftHandSide
+        case rightHandSide
+    }
+
+    // MARK: - Math Equation
+
+    private var mathEquation = MathEquation(leftHandSide: .zero)
+
+    // MARK: - Operation Side
+
+    private var operationSide: OperationSide = .leftHandSide
+
+    // MARK: - LCD Display text
+
+    var lcdDisplayText = "0"
+
     // MARK: - Extra Functions
 
     mutating func clearPressed() {
@@ -22,18 +39,28 @@ struct CalculatorEngine {
     // MARK: - Operations
 
     mutating func addPressed() {
+        mathEquation.operation = .add
+        operationSide = .rightHandSide
     }
 
     mutating func subtractPressed() {
+        mathEquation.operation = .subtract
+        operationSide = .rightHandSide
     }
 
     mutating func multiplyPressed() {
+        mathEquation.operation = .multiply
+        operationSide = .rightHandSide
     }
 
     mutating func dividePressed() {
+        mathEquation.operation = .divide
+        operationSide = .rightHandSide
     }
 
     mutating func equalsPressed() {
+        mathEquation.execute()
+        lcdDisplayText = mathEquation.result?.description ?? "Error"
     }
 
     // MARK: - Number Input
@@ -42,5 +69,13 @@ struct CalculatorEngine {
     }
 
     mutating func numberPressed(_ number: Int) {
+        let decimalNumber = Decimal(number)
+        lcdDisplayText = decimalNumber.description
+        switch operationSide {
+        case .leftHandSide:
+            mathEquation.leftHandSide = decimalNumber
+        case .rightHandSide:
+            mathEquation.rigthHandSide = decimalNumber
+        }
     }
 }
