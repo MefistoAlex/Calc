907b2be8cf5bdff28457ebd95b9ce55801c95cc4
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-12-12 17:08:48 +0100

    Created CalculatorEngine

---
+++ CalculatorEngine.swift
@@ -0,0 +1,46 @@
+//
+//  CalculatorEngine.swift
+//  Calc
+//
+//  Created by Alexandr Mefisto on 11.12.2022.
+//
+
+import Foundation
+
+struct CalculatorEngine {
+    // MARK: - Extra Functions
+
+    mutating func clearPressed() {
+    }
+
+    mutating func negatePressed() {
+    }
+
+    mutating func percentagePressed() {
+    }
+
+    // MARK: - Operations
+
+    mutating func addPressed() {
+    }
+
+    mutating func subtractPressed() {
+    }
+
+    mutating func multiplyPressed() {
+    }
+
+    mutating func dividePressed() {
+    }
+
+    mutating func equalsPressed() {
+    }
+
+    // MARK: - Number Input
+
+    mutating func decimalPressed() {
+    }
+
+    mutating func numberPressed(_ number: Int) {
+    }
+}
