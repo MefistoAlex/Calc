0c7a4792d61429241daf9a320b4809f80e36ef1e
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-12-21 11:48:36 -0500

    added saving and loading to UserDefaults

--- Buisness Logic Layer/ThemeManager.swift
+++ Buisness Logic Layer/ThemeManager.swift
@@ -11,10 +11,15 @@ final class ThemeManager {
 
     static let shared = ThemeManager()
 
+    private init() {
+        populateArrayOfThemes()
+        loadThemeIndex()
+    }
+
     // MARK: - Themes
 
     private(set) var themes = [CalculatorTheme]()
-
+    private var savedThemeIndex = 0
     private var savedTheme: CalculatorTheme?
     var currentTheme: CalculatorTheme {
         guard let savedTheme else {
@@ -23,9 +28,19 @@ final class ThemeManager {
         return savedTheme
     }
 
-    private init() {
-        populateArrayOfThemes()
+    // MARK: Next Theme
+
+    func moveToTheNextTheme() {
+        savedThemeIndex += 1
+        if savedThemeIndex > themes.count - 1 {
+            savedThemeIndex = 0
+        }
+
+        savedTheme = themes[savedThemeIndex]
+        saveThemeIndex()
     }
+
+    // MARK: Array of themes
 
     private func populateArrayOfThemes() {
         themes = [
@@ -40,5 +55,19 @@ final class ThemeManager {
             CalculatorTheme.orangeTheme,
             CalculatorTheme.washedOutTheme,
         ]
+    }
+
+    // MARK: - Seva & Load from disk
+
+    private func loadThemeIndex() {
+        if let previousIndex = UserDefaults.standard.object(forKey: "Mefisto.com.Calc.ThemeManager.savedThemeIndex") as? Int {
+            savedThemeIndex = previousIndex
+        }
+
+        savedTheme = themes[savedThemeIndex]
+    }
+
+    private func saveThemeIndex() {
+        UserDefaults.standard.set(savedThemeIndex, forKey: "Mefisto.com.Calc.ThemeManager.savedThemeIndex")
     }
 }
