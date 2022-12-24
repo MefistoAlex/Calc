71c610cce19e5a667c768c9c1b2566238c5f8de3
Alex Mefisto <alexandr.mefisto@gmail.com>
2022-12-23 16:32:18 -0800

    Added data store manager

---
+++ Data Storage Layer/DataStoreManager.swift
@@ -0,0 +1,29 @@
+//
+//  DataStoreManager.swift
+//  Calc
+//
+//  Created by Alexandr Mefisto on 21.12.2022.
+//
+
+import Foundation
+struct DataStoreManager {
+    // MARK: Unique key
+
+    let key: String
+
+    // MARK: Storing Data
+
+    func set(_ value: Any) {
+        UserDefaults.standard.set(value, forKey: key)
+    }
+
+    func getValue() -> Any? {
+        UserDefaults.standard.object(forKey: key)
+    }
+
+    // MARK: - Deleting Data
+
+    func deleteValue() {
+        UserDefaults.standard.removeObject(forKey: key)
+    }
+}
