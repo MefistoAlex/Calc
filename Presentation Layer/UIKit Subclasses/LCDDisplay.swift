e3c69b9d02ee8e7cbd422328e264495af42f641b
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-01-01 12:51:45 -0500

    created LCDDisplay subclass of UIView

---
+++ Presentation Layer/UIKit Subclasses/LCDDisplay.swift
@@ -0,0 +1,31 @@
+//
+//  LCDDisplay.swift
+//  Calc
+//
+//  Created by Alexandr Mefisto on 01.01.2023.
+//
+
+import UIKit
+
+class LCDDisplay: UIView {
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet var label: UILabel!
+    
+    // MARK: - Initialisers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    private func sharedInit() {
+        
+    }
+}
