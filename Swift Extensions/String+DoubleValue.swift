a7b30e3f43597ae58bd2424595be1cbf12073491
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-01-05 16:25:37 +0200

    created String+DoubleValue.swift

---
+++ Swift Extensions/String+DoubleValue.swift
@@ -0,0 +1,17 @@
+//
+//  String+DoubleValue.swift
+//  Calc
+//
+//  Created by Alexandr Mefisto on 05.01.2023.
+//
+
+import Foundation
+
+extension String {
+    
+    var doubleValue: Double? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter.number(from: self)?.doubleValue
+    }
+}
