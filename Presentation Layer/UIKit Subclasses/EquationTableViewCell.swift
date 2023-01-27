7bfa12f7157ea2bf9361c6af434cdabca4d527dd
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-01-27 21:19:36 +0200

    created EquationTableViewCell

---
+++ Presentation Layer/UIKit Subclasses/EquationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EquationTableViewCell.swift
+//  Calc
+//
+//  Created by Alexandr Mefisto on 27.01.2023.
+//
+
+import UIKit
+
+class EquationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
