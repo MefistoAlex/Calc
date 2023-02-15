b43320e7e6faa27dc776dbee3a7f8c17e4714d4e
Alex Mefisto <alexandr.mefisto@gmail.com>
2023-02-15 11:14:27 +0200

    created Constants

---
+++ Data Layer/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Calc
+//
+//  Created by Alexandr Mefisto on 15.02.2023.
+//
+
+import Foundation
+struct Constants {
+    static let decimalSymbol = "."
+    static let errorMessage = "Error"
+}
